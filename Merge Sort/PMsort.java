5a5587a53250a0b4d167292665f225b15abdec19
kevinliu1007 <kevinliu1007@gmail.com>
2017-01-27 18:26:29 -0600

    fix issues

--- Merge Sort/PMsort.java
+++ Merge Sort/PMsort.java
@@ -1,10 +1,9 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 
 public class PMsort extends RecursiveAction{
-	static final int SEQ_THRESHOLD = 2000000;
+	static final int SEQ_THRESHOLD = 200000;
 	
 	int low;
 	int high;
@@ -18,41 +17,43 @@ public class PMsort extends RecursiveAction{
 		tempMergArr = new int[High];
 	}
 	
-	protected void compute(){
+    	protected void compute(){
 		if((high - low) <= SEQ_THRESHOLD){
-			Arrays.sort(array, low, high);
-			Arrays.sort(tempMergArr, low, high);
+			Arrays.sort(array, low, high + 1);
 		}
 		else{
 			int mid = low + (high - low) / 2;
-			PMsort left = new PMsort(array, low, mid);
-			PMsort right = new PMsort(array, mid + 1, high);
+			PMsort left = new PMsort(array, low, mid + 1);
+			PMsort right = new PMsort(array, mid + 1, high + 1);
 			invokeAll(left, right);
 			mergeParts(low, mid, high);
 		}	
 	}
 	
-	private void mergeParts(int low, int mid, int high){
+    	private void mergeParts(int low, int mid, int high){
 		 
-        for (int i = low; i <= high; i++){
-            tempMergArr[i] = array[i];
-        }
-        int i = low;
-        int j = mid + 1;
-        int k = low;
-        while(i <= mid && j <= high){
-            if(tempMergArr[i] <= tempMergArr[j]){
-                array[k] = tempMergArr[i++];
-            } 
-            else{
-                array[k] = tempMergArr[j++];
-            }
-            k++;
-        }
-        while (i <= mid) {
-            array[k++] = tempMergArr[i++];
-        }
-    }
+        	for (int i = low; i <= high; i++){
+            		tempMergArr[i] = array[i];
+        	}
+        	int i = low;
+        	int j = mid + 1;
+        	int k = low;
+        	while(i <= mid && j <= high){
+            		if(tempMergArr[i] <= tempMergArr[j]){
+                		array[k] = tempMergArr[i++];
+            		} 
+            		else{
+                		array[k] = tempMergArr[j++];
+            		}
+            		k++;
+        	}
+        	while (i <= mid) {
+            		array[k++] = tempMergArr[i++];
+        	}
+		while(j <= high){
+	    		array[k++] = tempMergArr[j++];
+		}
+    	}
 }
 
 
