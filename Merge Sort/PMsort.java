35c35e2ebabd99e983f5c6351a33f519501654a8
kevinliu1007 <kevinliu1007@gmail.com>
2017-03-24 19:45:10 -0500

    refine spacing

--- Merge Sort/PMsort.java
+++ Merge Sort/PMsort.java
@@ -3,15 +3,15 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 
 
-public class PMsort extends RecursiveAction{
+public class PMsort extends RecursiveAction {
 	static final int SEQ_THRESHOLD = 200000;
 
-	int low;
-	int high;
-	int[] array;
-	int[] tempMergArr;
+	protected int low;
+	protected int high;
+	protected int[] array;
+	protected int[] tempMergArr;
 
-	sort(int[] arr, int Low, int High){
+	sort(int[] arr, int Low, int High) {
 		array = arr;
 		low = Low;
 		high = High - 1;
@@ -20,10 +20,9 @@ public class PMsort extends RecursiveAction{
 
 	protected void compute(){
 
-		if((high - low) <= SEQ_THRESHOLD){
+		if((high - low) <= SEQ_THRESHOLD) {
 			Arrays.sort(array, low, high + 1);
-		}
-		else{
+		} else {
 			int mid = low + (high - low) / 2;
 			PMsort left = new PMsort(array, low, mid + 1);
 			PMsort right = new PMsort(array, mid + 1, high + 1);
@@ -32,27 +31,30 @@ public class PMsort extends RecursiveAction{
 		}
 	}
 
-	private void mergeParts(int low, int mid, int high){
+	private void mergeParts(int low, int mid, int high) {
 
-		for (int i = low; i <= high; i++){
+		for (int i = low; i <= high; i++) {
 			tempMergArr[i] = array[i];
 		}
+		
 		int i = low;
 		int j = mid + 1;
 		int k = low;
-		while(i <= mid && j <= high){
+		
+		while(i <= mid && j <= high){ 
 			if(tempMergArr[i] <= tempMergArr[j]){
 				array[k] = tempMergArr[i++];
-			}
-			else{
+			} else {
 				array[k] = tempMergArr[j++];
 			}
 			k++;
 		}
-		while(i <= mid){
+		
+		while(i <= mid) {
 			array[k++] = tempMergArr[i++];
 		}
-		while(j <= high){
+		
+		while(j <= high) {
 			array[k++] = tempMergArr[j++];
 		}
 	}
